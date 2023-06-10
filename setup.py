58bb40a31dd638ab78b5d7ca4d757d241ba34de9
feiticeiro-tec <silviohenriquecruzdasilva@gmail.com>
2023-06-10 21:48:39 +0000

    add log basic e mudança de versão no setup para deploy

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='flask-dba',
-    version='1.0.0',
+    version='1.1.0',
     url='https://github.com/feiticeiro-tec/flask_dba',
     license='BSD3',
     author='Silvio Henrique Cruz Da Silva',
